484a81ed818f406a6ecb24cff3ad2e677ec459b5
Christopher Sharkey <ev2900@gmail.com>
2023-05-29 18:50:50 -0400

    updates

--- 2_Glue_Iceberg_Initial_Load/1_initial_load.py
+++ 2_Glue_Iceberg_Initial_Load/1_initial_load.py
@@ -13,9 +13,18 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# Update the S3 bucket name variable. Do NOT include s3:// prefix or a trailing /
+s3_bucket_name = "<s3_bucket_name>"
+
+# You Glue job MUST 
+# 1. Use Glue version 4.0
+# 2. Job parameter key = --datalake-formats , value = iceberg
+# 3. Job parameter key = -- conf            , value = spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://<bucket_name>/  --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO
+#    re. ^ (#3) make sure you update <s3_bucket>
+
 dynamic_frame_0 = glueContext.create_dynamic_frame_from_options(
     connection_type = "s3",
-    connection_options= {"paths": ["s3://mongodb-dhdkre3/initial_load/2_mongoDB_dump.json"]},
+    connection_options= {"paths": ["s3://" + s3_bucket_name "/initial_load/"]},
     format = "json"
 )
 
--- 4_Glue_Iceberg_Change_Stream/1_incremental.py
+++ 4_Glue_Iceberg_Change_Stream/1_incremental.py
@@ -18,10 +18,19 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# Update the S3 bucket name variable. Do NOT include s3:// prefix or a trailing /
+s3_bucket_name = "<s3_bucket_name>"
+
+# You Glue job MUST 
+# 1. Use Glue version 4.0
+# 2. Job parameter key = --datalake-formats , value = iceberg
+# 3. Job parameter key = -- conf            , value = spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://<bucket_name>/  --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO
+#    re. ^ (#3) make sure you update <s3_bucket>
+
 # Read in the S3 copy of the data that needs to have the changes applied to it
 user_dynamic_frame_0 = glueContext.create_dynamic_frame_from_options(
     connection_type = "s3",
-    connection_options= {"paths": ["s3://mongodb-dhdkre3/initial_load/2_mongoDB_dump.json"]},
+    connection_options= {"paths": ["s3://" + s3_bucket_name + "/initial_load/"]},
     format = "json"
 )
 
@@ -30,7 +39,7 @@ user_data_frame_0 = user_dynamic_frame_0.toDF()
 # Read in the MongoDB change stream data
 change_stream_dynamic_frame_0 = glueContext.create_dynamic_frame_from_options(
     connection_type = "s3",
-    connection_options= {"paths": ["s3://mongodb-dhdkre3/change_stream/"]},
+    connection_options= {"paths": ["s3://" + s3_bucket_name + "/change_stream/"]},
     format = "json"
 )
 
@@ -53,13 +62,18 @@ delete_w_full_document = delete_w_full_document.withColumn("wallTime_timestamp",
 # Select only the columns needed from the previous joined dataframe
 delete_w_full_document = delete_w_full_document.select(col("cs_operationType").alias("operationType"), "_id", col("wallTime_timestamp").alias("wallTime"), "first_name", "last_name", "email", "gender", "address", "card", "married_status")
 
-# Change the wallTime data type in the update_insert dataframe from a string to a timestamp
-update_insert = update_insert.withColumn("wallTime_timestamp", col('wallTime').cast('timestamp'))
-update_insert = update_insert.select("operationType", "_id", col("wallTime_timestamp").alias("wallTime"), "first_name", "last_name", "email", "gender", "address", "card", "married_status")
+# If there are updates/inserts and deletes merge the two dataframes
+if len(update_insert.head(1)) > 0:
+    # Change the wallTime data type in the update_insert dataframe from a string to a timestamp
+    update_insert = update_insert.withColumn("wallTime_timestamp", col('wallTime').cast('timestamp'))
+    update_insert = update_insert.select("operationType", "_id", col("wallTime_timestamp").alias("wallTime"), "first_name", "last_name", "email", "gender", "address", "card", "married_status")
 
-# Union the update_insert dataframe with the delete_w_full_document dataframe
-change_stream_ready_to_merge = update_insert.union(delete_w_full_document).orderBy("wallTime")
+    # Union the update_insert dataframe with the delete_w_full_document dataframe
+    change_stream_ready_to_merge = update_insert.union(delete_w_full_document)
 
+else:
+    change_stream_ready_to_merge = delete_w_full_document
+    
 # Iceberg merg into statement
 change_stream_ready_to_merge.createOrReplaceTempView("updates")
 
